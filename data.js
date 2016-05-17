26027379e212de814ac06270ec069646e240bacf
Mairin Bailey <mairin.bailey@gmail.com>
2016-05-17 12:50:31 -0600

    remove wiki api code

---
+++ data.js
@@ -0,0 +1,109 @@
+var poses = [{
+  name: "Downward Facing Dog",
+  symptoms: [
+    "back",
+    "shoulders",
+    "legs",
+    "mind"
+  ],
+   directions: ["Come onto the floor on your hands and knees. Set your knees directly below your hips and your hands slightly forward of your shoulders. Spread your palms, and turn your toes under.", "Exhale and lift your knees away from the floor. Lengthen your tailbone away from the back of your pelvis. Against this resistance, lift the sitting bones toward the ceiling.", "Then with an exhalation, push your top thighs back and stretch your heels onto or down toward the floor. Straighten your knees but be sure not to lock them. Firm the outer thighs and roll the upper thighs inward slightly. Narrow the front of the pelvis.","Firm the outer arms and press the bases of the index fingers actively into the floor. From these two points lift along your inner arms from the wrists to the tops of the shoulders. Firm your shoulder blades against your back, then widen them and draw them toward the tailbone. Keep the head between the upper arms; don’t let it hang.", "Stay in this pose anywhere from 1 to 3 minutes. Then bend your knees to the floor with an exhalation and sit your hips back onto your heels to rest in Child’s Pose."],
+   img: "http://yogatouchmethod.com/wp-content/uploads/2015/08/Martia_-053.jpg"
+ },
+ {
+  name: "Mountain Pose",
+  symptoms: [
+    "back",
+    "legs"
+  ],
+  directions: ["Stand with the bases of your big toes touching, heels slightly apart. Lift and spread your toes and the balls of your feet, then lay them softly down on the floor. Rock back and forth and side to side. Gradually reduce this swaying to a standstill, with your weight balanced evenly on the feet.", "Firm your thigh muscles and lift the knee caps, without hardening your lower belly. Lift the inner ankles to strengthen the inner arches, then imagine a line of energy all the way up along your inner thighs to your groins, and from there through the core of your torso, neck, and head, and out through the crown of your head. Turn the upper thighs slightly inward. Lengthen your tailbone toward the floor and lift the pubis toward the navel.","Press your shoulder blades into your back, then widen them across and release them down your back. Without pushing your lower front ribs forward, lift the top of your sternum straight toward the ceiling. Widen your collarbones. Hang your arms beside the torso.","Balance the crown of your head directly over the center of your pelvis, with the underside of your chin parallel to the floor, throat soft, and the tongue wide and flat on the floor of your mouth. Soften your eyes.","Tadasana is usually the starting position for all the standing poses. But it’s useful to practice Tadasana as a pose in itself. Stay in the pose for 30 seconds to 1 minute, breathing easily."],
+  img: "http://media.yogajournal.com/wp-content/uploads/Tadasana.jpg"
+},
+{
+  name: "Staff Pose",
+  symptoms: [
+    "back",
+    "shoulders"
+  ],
+  directions: ["Sit on the floor with your legs together and extended in front of your torso. If your torso is leaning back, it may be because tight hamstrings are dragging the sitting bones toward the knees and the back of the pelvis toward the floor. It may be helpful to sit on a blanket or a bolster to lift the pelvis.","Sit towards the front of the sitting bones, and adjust the pubis and tail bone equidistant from the floor. Without hardening the belly, firm the thighs, press them down against the floor (or your support), rotate them slightly toward each other, and draw the inner groins toward the sacrum. Flex your ankles, pressing out through your heels.","To lengthen your front torso perpendicular to the floor, think of energy streaming upward from the pubis to the sternum, then down the back from the shoulders to the tail bone. Then imagine the tail lengthening into the floor.","Imagine your spine as the “staff” at the vertical core of your torso, rooted firmly in the Earth, the support and pivot of all you do. Hold the pose for one minute or longer."],
+  img: "http://media.yogajournal.com/wp-content/uploads/MC_211_OPEN_N_1.jpg"
+},
+{
+ name: "Tree Pose",
+ symptoms: [
+   "legs",
+   "shoulders",
+   "back"
+ ],
+ directions: ["Stand tall with your arms hanging softly at your side. Shift your weight slightly onto the left foot, keeping the inner foot firm to the floor, and bend your right knee. Reach down with your right hand and clasp your right ankle.", "Draw your right foot up and place the sole against the inner left thigh; if possible, press the right heel into the inner left groin, toes pointing toward the floor. The center of your pelvis should be directly over the left foot.", "Rest your hands on the top rim of your pelvis. Make sure the pelvis is in a neutral position, with the top rim parallel to the floor.", "Lengthen your tailbone toward the floor. Firmly press the right foot sole against the inner thigh and resist with the outer left leg. Press your hands together in Anjali Mudra. Gaze softly at a fixed point in front of you on the floor about 4 or 5 feet away.","Stay for 30 seconds to 1 minute. Step back to Tadasana with an exhalation and repeat for the same length of time with the legs reversed."],
+ img: "http://media.yogajournal.com/wp-content/uploads/yogapedia_270_01_450x450.jpg"
+},
+{
+name: "Child's Pose",
+symptoms: [
+  "hips",
+  "back",
+  "legs",
+  "mind"
+],
+directions: ["Kneel on the floor. Touch your big toes together and sit on your heels, then separate your knees about as wide as your hips.", "Exhale and lay your torso down between your thighs. Broaden your sacrum across the back of your pelvis and narrow your hip points toward the navel, so that they nestle down onto the inner thighs. Lengthen your tailbone away from the back of the pelvis while you lift the base of your skull away from the back of your neck.", "Lay your hands on the floor alongside your torso, palms up, and release the fronts of your shoulders toward the floor. Feel how the weight of the front shoulders pulls the shoulder blades wide across your back.", "Balasana is a resting pose. Stay anywhere from 30 seconds to a few minutes. Beginners can also use Balasana to get a taste of a deep forward bend, where the torso rests on the thighs. Stay in the pose from 1 to 3 minutes. To come up, first lengthen the front torso, and then with an inhalation lift from the tailbone as it presses down and into the pelvis."],
+img: "http://media.yogajournal.com/wp-content/uploads/266_hp_side06_4501.jpg"
+},
+{
+name: "Warrior II",
+symptoms: [
+ "legs",
+ "back"
+],
+directions: ["Stand tall with your arms hanging softly at your sides. With an exhalation, step or lightly jump your feet 3 1/2 to 4 feet apart. Raise your arms parallel to the floor and reach them actively out to the sides, shoulder blades wide, palms down.","Turn your right foot slightly to the right and your left foot out to the left 90 degrees. Align the left heel with the right heel. Firm your thighs and turn your left thigh outward so that the center of the left knee cap is in line with the center of the left ankle.","Exhale and bend your left knee over the left ankle, so that the shin is perpendicular to the floor. If possible, bring the left thigh parallel to the floor. Anchor this movement of the left knee by strengthening the right leg and pressing the outer right heel firmly to the floor.","Stretch the arms away from the space between the shoulder blades, parallel to the floor. Don’t lean the torso over the left thigh: Keep the sides of the torso equally long and the shoulders directly over the pelvis. Press the tailbone slightly toward the pubis. Turn the head to the left and look out over the fingers.","Stay for 30 seconds to 1 minute. Inhale to come up. Reverse the feet and repeat for the same length of time to the left."],
+img: "http://media.yogajournal.com/wp-content/uploads/245_hp_01_450.jpg"
+},
+{
+name: "Standing Forward Fold",
+symptoms: [
+ "back",
+ "neck"
+],
+description: ["Stand tall with hands on hips. Exhale and bend forward from the hip joints, not from the waist. As you descend draw the front torso out of the groins and open the space between the pubis and top sternum. As in all the forward bends, the emphasis is on lengthening the front torso as you move more fully into the position.", "If possible, with your knees straight, bring your palms or finger tips to the floor slightly in front of or beside your feet, or bring your palms to the backs of your ankles. If this isn’t possible, cross your forearms and hold your elbows. Press the heels firmly into the floor and lift the sitting bones toward the ceiling. Turn the top thighs slightly inward.","With each inhalation in the pose, lift and lengthen the front torso just slightly; with each exhalation release a little more fully into the forward bend. In this way the torso oscillates almost imperceptibly with the breath. Let your head hang from the root of the neck, which is deep in the upper back, between the shoulder blades.","Uttanasana can be used as a resting position between the standing poses. Stay in the pose for 30 seconds to 1 minute. It can also be practiced as a pose in itself.","Don’t roll the spine to come up. Instead bring your hands back onto your hips and reaffirm the length of the front torso. Then press your tailbone down and into the pelvis and come up on an inhalation with a long front torso."],
+img: "http://media.yogajournal.com/wp-content/uploads/261_hp_Standingforwardbend_Week18.jpg"
+},
+{
+name: "Savasana",
+symptoms: [
+ "mind"
+],
+description: ["In Savasana it’s essential that the body be placed in a neutral position. Sit on the floor with your knees bent, feet on the floor, and lean back onto your forearms. Lift your pelvis slightly off the floor and, with your hands, push the back of the pelvis toward the tailbone, then return the pelvis to the floor. Inhale and slowly extend the right leg, then the left, pushing through the heels. Release both legs, softening the groins, and see that the legs are angled evenly relative to the mid-line of the torso, and that the feet turn out equally. Narrow the front pelvis and soften (but don’t flatten) the lower back.","With your hands lift the base of the skull away from the back of the neck and release the back of the neck down toward the tailbone. If you have any difficulty doing this, support the back of the head and neck on a folded blanket. Broaden the base of the skull too, and lift the crease of the neck diagonally into the center of the head. Make sure your ears are equidistant from your shoulders.","Reach your arms toward the ceiling, perpendicular to the floor. Rock slightly from side to side and broaden the back ribs and the shoulder blades away from the spine. Then release the arms to the floor, angled evenly relative to the mid-line of torso. Turn the arms outward and stretch them away from the space between the shoulder blades. Rest the backs of the hands on the floor as close as you comfortably can to the index finger knuckles. Make sure the shoulder blades are resting evenly on the floor. Imagine the lower tips of the shoulder blades are lifting diagonally into your back toward the top of the sternum. From here, spread the collarbones.","In addition to quieting the physical body in Savasana, it’s also necessary to pacify the sense organs. Soften the root of the tongue, the wings of the nose, the channels of the inner ears, and the skin of the forehead, especially around the bridge of the nose between the eyebrows. Let the eyes sink to the back of the head, then turn them downward to gaze at the heart. Release your brain to the back of the head.","Stay in this pose for 5 minutes for every 30 minutes of practice. To exit, first roll gently with an exhalation onto one side, preferably the right. Take 2 or 3 breaths. With another exhalation press your hands against the floor and lift your torso, dragging your head slowly after. The head should always come up last."],
+img: "http://media.yogajournal.com/wp-content/uploads/CorpsePoseYoga.jpg"
+},
+{
+name: "Cobbler's Pose",
+symptoms: [
+ "hips",
+ "back"
+],
+description: ["Sit with your legs straight out in front of you, raising your pelvis on a blanket if your hips or groins are tight. Exhale, bend your knees, pull your heels toward your pelvis, then drop your knees out to the sides and press the soles of your feet together.","Bring your heels as close to your pelvis as you comfortably can. With the first and second finger and thumb, grasp the big toe of each foot. Always keep the outer edges of the feet firmly on the floor. If it isn’t possible to hold the toes, clasp each hand around the same-side ankle or shin.","Sit so that the pubis in front and the tailbone in back are equidistant from the floor. The perineum then will be approximately parallel to the floor and the pelvis in a neutral position. Firm the sacrum and shoulder blades against the back and lengthen the front torso through the top of the sternum.","Never force your knees down. Instead release the heads of the thigh bones toward the floor. When this action leads, the knees follow.","Stay in this pose anywhere from 1 to 5 minutes. Then inhale, lift your knees away from the floor, and extend the legs back to their original position."],
+img: "http://media.yogajournal.com/wp-content/uploads/garden_228_03_4501.jpg"
+},
+{
+name: "Hands at Heart Center",
+symptoms: [
+ "wrists"
+],
+description: ["Sit comfortably or stand in Tadasana. Inhale and bring your palms together. Rest the thumbs lightly on your sternum.","Press the hands firmly but evenly against each other. Make sure that one hand (usually your right hand if you are right-handed, your left if left-handed) doesn’t dominate the other. If you find such imbalance, release the dominant hand slightly but don’t increase the pressure of the non-dominant hand.","Bow your head slightly, drawing the crease of the neck toward the center of your head. Lift your sternum into your thumbs and lengthen down along the back of the armpits, making the back elbows heavy.", "Practicing Anjali Mudra is an excellent way to induce a meditative state of awareness. Start your practice sitting in meditation in Anjali Mudra for 5 minutes. You can use this hand position and contemplate the “sun” or light of awareness the yogis say is resident in your heart."],
+img:"http://media.yogajournal.com/wp-content/uploads/2894-161.jpg"
+},
+{
+name: "Upward Facing Dog",
+symptoms: [
+ "hips",
+ "back",
+ "wrists"
+],
+description: ["Lie prone on the floor. Stretch your legs back, with the tops of your feet on the floor. Bend your elbows and spread your palms on the floor beside your waist so that your forearms are relatively perpendicular to the floor.","Inhale and press your inner hands firmly into the floor and slightly back, as if you were trying to push yourself forward along the floor. Then straighten your arms and simultaneously lift your torso up and your legs a few inches off the floor on an inhalation. Keep the thighs firm and slightly turned inward, the arms firm and turned out so the elbow creases face forward.","Press the tailbone toward the pubis and lift the pubis toward the navel. Narrow the hip points. Firm but don’t harden the buttocks.","Firm the shoulder blades against the back and puff the side ribs forward. Lift through the top of the sternum but avoid pushing the front ribs forward, which only hardens the lower back. Look straight ahead or tip the head back slightly, but take care not to compress the back of the neck and harden the throat.","Urdhva Mukha Svanasana is one of the positions in the traditional Sun Salutation sequence. You can also practice this pose individually, holding it anywhere from 15 to 30 seconds, breathing easily. Release back to the floor or lift into Downward Facing Dog with an exhalation."],
+img: "http://media.yogajournal.com/wp-content/uploads/230_hp_07_fnl_450.jpg"
+}
+];
+
+function doSomething(){
+console.log(poses)
+};
+doSomething();
